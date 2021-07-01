26692959a802afb031e73d536752a3c7c6840e7d
Mansurjon Muhammadovich Kamolov <64670025+Mansurjon1112@users.noreply.github.com>
2021-07-01 15:01:21 +0500

    Update boolean35.py

--- boolean35.py
+++ boolean35.py
@@ -12,6 +12,4 @@ x1=int(input('x1='))
 y1=int(input('y1='))
 x2=int(input('x2='))
 y2=int(input('y2='))
-#ikkalasini birlashtirish kerak .
-print(x1%2==0 and y1%2==0 and x2%2==0 and y2%2==0 or x1%2==1 and y1%2==1 and  x2%2==1 and y2%2==1)
-print(x1%2==1 and y1%2==0 and x2%2==1 and y2%2==0 or x1%2==0 and y1%2==1 and  x2%2==0 and y2%2==1)
+print(x1+y1) % 2 == (x2+y2) % 2 )
