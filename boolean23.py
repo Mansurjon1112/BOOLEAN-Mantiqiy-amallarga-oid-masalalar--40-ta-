0dd438bb1bc7b1a0a895dce0ac3b6b2db7455af0
Mansurjon Muhammadovich Kamolov <64670025+Mansurjon1112@users.noreply.github.com>
2021-07-02 18:42:06 +0500

    Update boolean23.py

--- boolean23.py
+++ boolean23.py
@@ -9,8 +9,14 @@ Created on Wed Jun 30 20:19:14 2021
 """
 
 a=int(input('A='))
+#1-usul
 r1=a%10
 r2=a%100//10
 r3=a//100
 b=r1*100+r2*10+r3
 print(a==b)
+
+#2-usul
+yuz = a // 100
+birlar = a%10
+print(yuz==birlar)
